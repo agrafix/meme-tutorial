5a2de4ff1bf8a4f973eab636e0868d95464018e3
Stefan Wehr <wehr@cp-med.com>
2017-08-02 14:31:13 +0200

    Implement cmdline version

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
 import MemeGen
+import System.Environment
+import System.Exit
+import Control.Monad
+import qualified Data.ByteString as BS
 
 main :: IO ()
-main = undefined createMeme
+main =
+    do args <- getArgs
+       when ("--help" `elem` args) usage
+       case args of
+         [img, upperText, lowerText, out] ->
+             do bs <- BS.readFile img
+                resultBs <- createMeme bs upperText lowerText
+                BS.writeFile out resultBs
+         _ -> usage
+    where
+      usage =
+          do putStrLn "USAGE: memegen INPUT_IMAGE.jpg UPPER_TEXT LOWER_TEXT OUTPUT_IMAGE.jpg"
+             exitWith (ExitFailure 1)
--- src/MemeGen.hs
+++ src/MemeGen.hs
@@ -3,7 +3,28 @@ module MemeGen
     )
 where
 
+import qualified Graphics.GD as GD
 import qualified Data.ByteString as BS
 
+textColor :: GD.Color
+textColor = GD.rgb 255 255 255
+
+textSize :: Double
+textSize = 32.0
+
 createMeme :: BS.ByteString -> String -> String -> IO BS.ByteString
-createMeme = undefined
+createMeme imgBs upperText lowerText =
+    do img <- GD.loadJpegByteString imgBs
+       (imgW, imgH) <- GD.imageSize img
+       _ <- GD.useFontConfig True
+       drawText img imgW upperText (\textH -> textH + 10)
+       drawText img imgW lowerText (\_ -> imgH -20)
+       GD.saveJpegByteString 100 img
+    where
+      drawText img imgW txt offset =
+          do (_, (lrx, lry), _, (ulx, uly))
+                 <- GD.measureString "sans" textSize 0.0 (0, 0) txt textColor
+             let (textW, textH) = (1 + lrx - ulx, 1 + lry - uly)
+                 pos = (imgW `div` 2 - textW `div` 2, offset textH)
+             _ <- GD.drawString "sans" textSize 0.0 pos txt textColor img
+             return ()
